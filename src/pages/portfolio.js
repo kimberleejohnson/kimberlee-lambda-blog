cd5a0dafbc3f1f5bd39cd94db716f3de5546307a
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-09-01 19:28:13 -0700

    Added description of Ride for Life project

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,6 +1,9 @@
 import React from "react"
 import Layout from "../components/layout"
+
+// Portfolio screenshots
 import sautiScreenshot from "../images/portfolio-images/sauti_screenshot.png"; 
+import rideForLifeScreenshot from "../images/portfolio-images/rideForLife_screenshot.png"; 
 
 export default () => (
     <Layout>  
@@ -20,5 +23,21 @@ export default () => (
           <a href="https://drive.google.com/file/d/1nVPaBB6P-KiCa9X1PwQmWm8UsQ5UzyVz/view?usp=sharing"> Case Study </a> 
         </p>
     </div>
+
+    <div>
+        <h3>Ride for Life</h3>
+        <img
+            src={rideForLifeScreenshot}
+            alt="Mobile view of the Ride For Life marketing page, featuring a woman holding a baby and a sign in button."
+        
+        /> 
+        <p>A marketing page for Ride for Life, an app that allows pregnant mothers in Uganda to take safe, affordable boda-boda rides to medical appointments. I used media queries to design and build this mobile-first site. </p>
+        <p><b>Stack</b>: HTML, CSS, LESS, JavaScript.</p>
+        <p>
+          <a href="https://github.com/build-rideForLife/build_rideForLife_marketingA"> Code </a> | 
+          <a href="https://rideforlifeapp.netlify.com"> Live site </a> 
+        </p>
+    </div>
+
     </Layout>
       )
