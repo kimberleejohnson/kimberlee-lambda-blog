b3ffca5237b31176d46dea089ac244d858885e78
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-10-05 16:34:12 -0700

    Updated the text of my intro paragraphs

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,22 +4,26 @@ import kimberleelandsend from "../images/kimberleelandsend.jpg"
 
 export default () => (
   <Layout>  
-    <h3>Hi there! I'm a fullstack software engineer looking for my next adventure.</h3>
+    <h3>Hi there! I'm a full stack software engineer looking for my next adventure.</h3>
     
-    <p> Have a look at some of the things I've built, blogged, and done for work.</p>
+    <p> Have a look at some of the <a href="https://kimberlee.dev/portfolio">software</a> I've written, <a href= "https://kimberlee.dev/console-blog">projects</a> I've blogged about, and other <a href="https://kimberlee.dev/resume">experiences</a> I've had.</p>
 
-    <p> When I'm not programming, I run a lot, try to read even more, and am ever-perfecting my mandolin and ukulele skills.</p> 
-    
-    <img
-        src={kimberleelandsend}
-        alt="Kimberlee Johnson's headshot"
-      />
+    <p> If I'm not programming, I'm probably out on a run training for a race, checking out books from the library, or practicing mandolin and ukulele.</p> 
+
+    <p> I'd love to meet you! Feel free to say hello.</p> 
 
     <p>
       <a href="https://github.com/kimberleejohnson"> Github </a> | 
       <a href="https://www.linkedin.com/in/kimberleejohnson/"> LinkedIn </a> |
       <a href="https://twitter.com/kimeejohnson"> | Twitter </a> 
     </p>
+    
+    <img
+        src={kimberleelandsend}
+        alt="Kimberlee Johnson's headshot"
+      />
+
+    
   </Layout>
     )
     
