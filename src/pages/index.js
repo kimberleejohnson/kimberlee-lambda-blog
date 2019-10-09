cb95623a9aa5db93edecb61b5282c3f9eed2df15
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-10-09 14:11:22 -0700

    Rearranged home page order

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,7 @@ export default () => (
   <Layout>  
     <h3>Hi there! I'm a full stack software engineer looking for my next adventure.</h3>
 
-    <p> Have a look at some of the <a href="https://kimberlee.dev/portfolio">software</a> I've written, <a href= "https://kimberlee.dev/console-blog">projects</a> I've blogged about, and other <a href="https://kimberlee.dev/resume">experiences</a> I've had.</p>
-
-    <img
-        src={kimberleelandsend}
-        alt="Kimberlee Johnson's headshot"
-    />
+    <p> Have a look at some of the <a href="https://kimberlee.dev/portfolio">things</a> I've built, <a href= "https://kimberlee.dev/console-blog">projects</a> I've blogged about, and other <a href="https://kimberlee.dev/resume">experiences</a> I've had.</p>
 
     <p> If I'm not programming, I'm probably out on a run training for a race, checking out books from the library, or practicing mandolin and ukulele.</p> 
 
@@ -26,6 +21,11 @@ export default () => (
 
       <a href="https://dev.to/kimberleejohnson"><img class="icon-img" src="https://img.icons8.com/metro/26/000000/document.png"></img></a>
     </div>
+
+    <img
+        src={kimberleelandsend}
+        alt="Kimberlee Johnson's headshot"
+    />
 
     
 
