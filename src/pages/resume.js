929aad6c48dfb4b569c983d345af1afc9de0e674
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-05-04 14:15:02 -0700

    fix: styling should be set on mobile now

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,18 +1,19 @@
 import React from "react"
 import Layout from "../components/layout"
-import {Helmet} from "react-helmet"
+import { Helmet } from "react-helmet"
 
 export default () => (
-  <Layout>
+  <div>
     <Helmet>
       <script
         async
         src="https:&#x2F;&#x2F;sdk.canva.com&#x2F;v1&#x2F;embed.js"
-      ></script>
+      />
     </Helmet>
 
     <div>
-      <div
+      <Layout />
+      <a
         className="canva-embed"
         data-design-id="DADlx_k289w"
         data-height-ratio="1.2941"
@@ -20,17 +21,12 @@ export default () => (
           padding: "129.4118% 5px 5px 5px",
           background: "rgba(0,0,0,0.03)",
           borderRadius: 8,
+          maxWidth: "auto",
         }}
-      />
-      <a
         href="https://www.canva.com/design/DADlx_k289w/view?utm_content=DADlx_k289w&utm_campaign=designshare&utm_medium=embeds&utm_source=link"
         target="_blank"
         rel="noopener"
-      ></a>
+      />
     </div>
-  </Layout>
+  </div>
 )
-
-
-
-
