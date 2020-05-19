c3389dab524bba477ebe84b5e00219d952bee802
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-05-19 16:15:20 -0700

    fix: enclosed resume embed in layout tag to fix styling

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -11,24 +11,20 @@ export default () => (
       />
     </Helmet>
 
-    <div>
-      <Layout />
+    <Layout>
       <a
         className="canva-embed"
         data-design-id="DADlx_k289w"
         data-height-ratio="1.2941"
         style={{
-          padding: "129.4118% 5px 5px 5px",
+          //padding: "129.4118% 5px 5px 5px",
           background: "rgba(0,0,0,0.03)",
           borderRadius: 8,
-          maxWidth: '100%', 
-          height: 'auto', 
-          margin: '0 auto'
         }}
         href="https://www.canva.com/design/DADlx_k289w/view?utm_content=DADlx_k289w&utm_campaign=designshare&utm_medium=embeds&utm_source=link"
         target="_blank"
         rel="noopener"
       />
-    </div>
+    </Layout>
   </div>
 )
