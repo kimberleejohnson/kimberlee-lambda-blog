8222f1c1443764c345a31d78cff0484fe24b0f81
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-06-02 18:21:11 -0700

    Attempting to account for window not being available on server side

--- src/pages/startCall.js
+++ src/pages/startCall.js
@@ -1,18 +1,20 @@
 export default function startCall() {
   let room = { url: "https://kimberlee.daily.co/hello" }
 
-  let callFrame = window.DailyIframe.createFrame({
-    iframeStyle:{
-      top: '0',
-      left: '0', 
-      width: '100%',
-      height: '100%',
-      position: 'fixed'
-    }
-  });
-
-  callFrame.join({
-    url: room.url,
-    showLeaveButton: true,
-  })
+  if (typeof window !== 'undefined') {
+    let callFrame = window.DailyIframe.createFrame({
+      iframeStyle:{
+        top: '0',
+        left: '0', 
+        width: '100%',
+        height: '100%',
+        position: 'fixed'
+      }
+    });
+  
+    callFrame.join({
+      url: room.url,
+      showLeaveButton: true,
+    })
+  }
 }
